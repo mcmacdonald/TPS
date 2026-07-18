8aaf14018f9d2857f685cee2a6653bdc550ca45c
Mitch Macdonald <35157616+mcmacdonald@users.noreply.github.com>
2026-07-18 13:43:52 -0300

    updated to include data aggregation
    update

--- syntax/01.R
+++ syntax/01.R
@@ -1,6 +1,7 @@
 # This .R syntax file loads and joins homicides and shootings data 
 
 
+
 # don't run 
 # install packages
 # install.packages(
@@ -8,7 +9,8 @@
     # "tidyr", 
     # "stringr", 
     # "magrittr",
-    # "ggplot2"
+    # "readr",
+    # "readxl",
     # )
   # )
 
@@ -95,7 +97,107 @@ joined$DEATH[joined$DEATH > 1] <- 1 # values range 0, 1, 2, 3
 # cross-tabulation of homicides and deaths recorded in the shootings data
 table(joined$HOMICIDE, joined$DEATH) # good sign - strong agreement in the coding
 
+# calculate baseline probability of shooting resulting in a homicide, controlling for annual variation 
+logit <- stats::glm(HOMICIDE ~ 1 + factor(OCC_YEAR), family = "binomial", data = joined)
 
+# inverse logit function to calculate probability
+inv_logit <- function(x){ 1 / (1 + exp(-x)) }
+
+# probability of homicide
+p <- inv_logit(logit$coefficients[[1]])
+message(round(p * 100, digits = 0),"% or roughly 1-in-", round(1/p, digits = 0), " shootings result in a homicide, controlling for annual variation in shootings.")
+
+
+
+# aggregate data to the neighborhood level of analysis -----------------------------------------------
+
+# join column that denotes shootings
+joined$SHOOTINGS <- 1
+
+# select columns
+joined <- joined %>% dplyr::select(
+  HOMICIDE, 
+  SHOOTINGS, 
+  OCC_YEAR, 
+  # OCC_MONTH, 
+  # OCC_DAY, 
+  # OCC_DOW, 
+  # OCC_DOY, 
+  DIVISION, 
+  HOOD_158, 
+  NEIGHBOURHOOD_158, 
+  LONG_WGS84, 
+  LAT_WGS84
+  )
+
+# rename columns
+colnames(joined) <- c("homicides",
+                      "shootings",
+                      "year",
+                      # "month",
+                      # "day",
+                      # "dow",
+                      # "doy",
+                      "division",
+                      "hood",
+                      "neighbourhood",
+                      "lon",
+                      "lat"
+                      )
+
+# strip numeric strings from neighborhoods
+joined$neighbourhood <- stringr::str_replace(joined$neighbourhood, "\\s\\(\\d+\\)", "")
+
+# aggregate homicides and shootings by neighborhood
+joined <- joined %>% dplyr::group_by(year, neighbourhood) %>%
+  dplyr::summarise(
+    homicides = sum(homicides, na.rm = TRUE),
+    shootings = sum(shootings, na.rm = TRUE),
+    # pull the first case in the group for the other columns
+    dplyr::across(c(division, hood, lon, lat), ~ dplyr::first(.x)),
+    # drop groups
+    .groups = "drop"
+    )
+
+
+
+# get list of 158 census designated neighborhoods in Toronto
+# hyperlink to data: https://www.toronto.ca/city-government/data-research-maps/neighbourhoods-communities/neighbourhood-profiles/about-toronto-neighbourhoods/
+
+# 2. Create a temporary file path on your machine
+tmp <- tempfile(fileext = ".xlsx")
+
+readr::write_file(
+  readr::read_file_raw(
+    "https://ckan0.cf.opendata.inter.prod-toronto.ca/dataset/6e19a90f-971c-46b3-852c-0c48c436d1fc/resource/19d4a806-7385-4889-acf2-256f1e079060/download/neighbourhood-profiles-2021-158-model.xlsx"),
+  tmp
+  )
+neighbourhood_profiles <- readxl::read_excel(tmp)
+
+# names of Toronto neighborhoods
+neighbourhoods <- colnames(neighbourhood_profiles)
+neighbourhoods <- neighbourhoods[-1] # drop first element
+
+# as data frame
+neighbourhoods <- as.data.frame(neighbourhoods); colnames(neighbourhoods) <- c("neighbourhood")
+
+# complete grid skeleton for the 158 neighborhoods for all available years
+grid_skeleton <- tidyr::crossing(
+  neighbourhood = neighbourhoods$neighbourhood,
+  year = 2004:2026
+  )
+
+# join aggregated data to this skeleton
+joined <- grid_skeleton %>%
+  dplyr::left_join(joined, by = c("neighbourhood", "year")) %>%
+    dplyr::mutate( # 3. Clean up the missing data fields
+    homicides = tidyr::replace_na(homicides, 0),
+    shootings = tidyr::replace_na(shootings, 0)
+    ) %>%
+  # fill in any missing information about the neighborhoods
+  dplyr::group_by(neighbourhood) %>% 
+  tidyr::fill(division, hood, lon, lat, .direction = "downup") %>%
+  dplyr::ungroup()
 
 
 
